67e98185cd0efd6f9eb87f63dbd9453cac308132
Virgile Gerecke <virgile.gerecke@unistra.fr>
2020-04-21 15:55:06 +0200

    ♻️ Refactored code

--- test.py
+++ test.py
@@ -7,56 +7,50 @@ import json
 
 
 class TestTips(unittest.TestCase):
-    def _common(self, content):
+    def _result(self, content):
         f = StringIO()
         with redirect_stdout(f):
             provider.main(content)
-        return json.loads(f.getvalue())
+        return {i['label']: i for i in json.loads(f.getvalue()) if 'label' in i}
+
+    def _common_ok(self, content, key, value):
+        result = self._result(content)
+        self.assertIn(key, result)
+        self.assertEqual(value, result[key]['value'])
+
+    def _common_ko(self, content, key):
+        result = self._result(content)
+        self.assertNotIn(key, result)
 
     def test_authiris_ok(self):
-        result = self._common('toto')
-        self.assertIn('Authiris toto', [i['label'] for i in result if 'label' in i])
-        self.assertEqual('https://authiris.unistra.fr/authiris/bin/cptedit?uti=toto',
-                         {i['label']: i for i in result if 'label' in i}['Authiris toto']['value'])
+        self._common_ok('toto', 'Authiris toto', 'https://authiris.unistra.fr/authiris/bin/cptedit?uti=toto')
 
     def test_authiris_ko(self):
-        result = self._common('héhé')
-        self.assertNotIn('Authiris toto', [i['label'] for i in result if 'label' in i])
+        self._common_ko('héhé', 'Authiris héhé')
 
     def test_wikitionnaire_ok(self):
-        result = self._common('héhé')
-        self.assertIn('Wikitionnaire héhé', [i['label'] for i in result if 'label' in i])
-        self.assertEqual('https://fr.wiktionary.org/w/index.php?search=h%C3%A9h%C3%A9',
-                         {i['label']: i for i in result if 'label' in i}['Wikitionnaire héhé']['value'])
+        self._common_ok('héhé', 'Wikitionnaire héhé', 'https://fr.wiktionary.org/w/index.php?search=h%C3%A9h%C3%A9')
 
     def test_wikitionnaire_ko(self):
-        result = self._common('expression régulière')
-        self.assertNotIn('Wikitionnaire expression régulière', [i['label'] for i in result if 'label' in i])
+        self._common_ko('expression régulière', 'Wikitionnaire expression régulière')
 
     def test_wikipedia(self):
-        result = self._common('expression régulière')
-        self.assertIn('Wikipedia expression régulière', [i['label'] for i in result if 'label' in i])
-        self.assertEqual('https://fr.wikipedia.org/w/index.php?title=expression+r%C3%A9guli%C3%A8re',
-                         {i['label']: i for i in result if 'label' in i}['Wikipedia expression régulière']['value'])
+        self._common_ok('expression régulière', 'Wikipedia expression régulière',
+                        'https://fr.wikipedia.org/w/index.php?title=expression+r%C3%A9guli%C3%A8re')
 
     def test_url(self):
-        result = self._common('toto<a href="https://www.unistra.fr">Unistra</a>tutu titi')
-        self.assertIn('Open https://www.unistra.fr', [i['label'] for i in result if 'label' in i])
-        self.assertEqual('https://www.unistra.fr',
-                         {i['label']: i for i in result if 'label' in i}['Open https://www.unistra.fr']['value'])
+        self._common_ok('toto<a href="https://www.unistra.fr">Unistra</a>tutu titi', 'Open https://www.unistra.fr',
+                        'https://www.unistra.fr')
 
     def test_timestamp(self):
-        result = self._common('1587476268')
-        self.assertIn('Timestamp', [i['label'] for i in result if 'label' in i])
-        self.assertEqual('21/04/2020 15:37:48',
-                         {i['label']: i for i in result if 'label' in i}['Timestamp']['value'])
+        self._common_ok('1587476268', 'Timestamp',
+                        '21/04/2020 15:37:48')
 
     def test_jwt(self):
-        result = self._common(
-            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzb21lIjoicGF5bG9hZCJ9.Joh1R2dYzkRvDkqv3sygm5YyK8Gi4ShZqbhK2gxcs2U')
-        self.assertIn('JWT', [i['label'] for i in result if 'label' in i])
-        self.assertEqual('[{"typ": "JWT", "alg": "HS256"}, {"some": "payload"}]',
-                         {i['label']: i for i in result if 'label' in i}['JWT']['value'])
+        self._common_ok(
+            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzb21lIjoicGF5bG9hZCJ9.Joh1R2dYzkRvDkqv3sygm5YyK8Gi4ShZqbhK2gxcs2U',
+            'JWT',
+            '[{"typ": "JWT", "alg": "HS256"}, {"some": "payload"}]')
 
 
 if __name__ == '__main__':
